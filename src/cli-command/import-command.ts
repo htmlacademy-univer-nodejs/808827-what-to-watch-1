483804c8cd3cebe2ff5b2c11942d4e27ce1e9e94
NaNeG <kolosovg02@hotmail.com>
2022-10-09 17:20:09 +0500

    added generate command, updated import command

---
+++ src/cli-command/import-command.ts
@@ -0,0 +1,27 @@
+import TSVFileReader from "../common/file-reader/tsv-file-reader.js";
+import { createMovie, getErrorMessage } from "../utils/common.js";
+import { CliCommandInterface } from "./cli-command.interface.js";
+
+export default class ImportCommand implements CliCommandInterface {
+  public readonly name = "--import";
+  private onLine(line: string) {
+    const movie = createMovie(line);
+    console.log(movie);
+  }
+
+  private onComplete(count: number) {
+    console.log(`${count} rows imported.`);
+  }
+
+  public async execute(filename: string): Promise<void> {
+    const fileReader = new TSVFileReader(filename.trim());
+    fileReader.on("line", this.onLine);
+    fileReader.on("end", this.onComplete);
+
+    try {
+      await fileReader.read();
+    } catch (err) {
+      console.log(`Can't read the file: ${getErrorMessage(err)}`);
+    }
+  }
+}
---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,18 @@
+import { Genre } from "./genre.type";
+
+export type MockData = {
+  titles: string[];
+  descriptions: string[];
+  genres: Genre[];
+  videoPreviewLinks: string[];
+  videoLinks: string[];
+  actors: string[];
+  directors: string[];
+  users: string[];
+  emails: string[];
+  avatarPaths: string[];
+  passwords: string[];
+  posterPaths: string[];
+  backgroundPicturePaths: string[];
+  backgroundColors: string[];
+};
